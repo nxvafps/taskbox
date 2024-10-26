8e84b525ee0776c984139fe2e96723c85aab9ad0
nxvafps <oliverquirk11@gmail.com>
2024-10-26 20:23:14 +0100

    Completed lesson 1

---
+++ src/components/Task.jsx
@@ -0,0 +1,64 @@
+import PropTypes from "prop-types";
+
+export default function Task({
+  task: { id, title, state },
+  onArchiveTask,
+  onPinTask,
+}) {
+  return (
+    <div className={`list-item ${state}`}>
+      <label
+        htmlFor={`archiveTask-${id}`}
+        aria-label={`archiveTask-${id}`}
+        className="checkbox"
+      >
+        <input
+          type="checkbox"
+          disabled={true}
+          name="checked"
+          id={`archiveTask-${id}`}
+          checked={state === "TASK_ARCHIVED"}
+        />
+        <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
+      </label>
+
+      <label htmlFor={`title-${id}`} aria-label={title} className="title">
+        <input
+          type="text"
+          value={title}
+          readOnly={true}
+          name="title"
+          id={`title-${id}`}
+          placeholder="Input title"
+        />
+      </label>
+      {state !== "TASK_ARCHIVED" && (
+        <button
+          className="pin-button"
+          onClick={() => onPinTask(id)}
+          id={`pinTask-${id}`}
+          aria-label={`pinTask-${id}`}
+          key={`pinTask-${id}`}
+        >
+          <span className={`icon-star`} />
+        </button>
+      )}
+    </div>
+  );
+}
+
+Task.propTypes = {
+  //composition of the task
+  task: PropTypes.shape({
+    //id
+    id: PropTypes.string.isRequired,
+    //title
+    title: PropTypes.string.isRequired,
+    //current state
+    state: PropTypes.string.isRequired,
+  }),
+  //event to change task to archived
+  onArchiveTask: PropTypes.func,
+  //event to change task to pinned
+  onPinTask: PropTypes.func,
+};
